84d53ac433c9f39dabdbcfbd85ac33628ccae702
oovelo <hello@oovelo.com>
2026-07-14 11:13:48 +0900

    Register as a login item on launch
    The app's whole purpose is continuous monitoring, so it should come back
    after logout/reboot without the user manually relaunching it via Xcode.

--- ypower/AppDelegate.swift
+++ ypower/AppDelegate.swift
@@ -1,7 +1,11 @@
 import AppKit
+import ServiceManagement
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
+        if SMAppService.mainApp.status != .enabled {
+            try? SMAppService.mainApp.register()
+        }
         MenuBarViewModel.shared.start()
     }
 }
